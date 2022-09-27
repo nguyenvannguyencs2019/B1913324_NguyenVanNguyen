abe685cfa25a24a00fb5b4e1c378822e0a63355e
nguyenvannguyencs2019 <nguyenvannguyencs2019@gmail.com>
2022-09-27 14:05:05 +0700

    Cai dat cac handler truy xuat CSDL

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,9 +1,7 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const {MongoClient} = require("mongodb");
 class MongoDB{
     static connect = async(uri) => {
-        if(this.client)
-            return this.client;
+        if(this.client) return this.client;
         this.client = await MongoClient.connect(uri);
         return this.client;
     };
